d2270f6993366400f35273f619275932653c69d1
navneeth-mn <navneethnavu108@gmail.com>
2023-09-13 20:10:15 +0530

    endpoint
    change

--- eventapp/urls.py
+++ eventapp/urls.py
@@ -18,6 +18,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index, name='index'),
+    path('/event',views.index, name='index'),
     path('event-details/<int:pk>',views.eventdetail,name='eventdetail')
 ]
