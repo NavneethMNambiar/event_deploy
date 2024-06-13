e3edb794854273931229d944f7c596da27c8118e
navneeth-mn <121511892+NavneethMNambiar@users.noreply.github.com>
2024-06-13 10:16:19 +0530

    Update settings.py

--- eventmanagementproject/settings.py
+++ eventmanagementproject/settings.py
@@ -126,9 +126,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR,'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR,'static'),
+# )
 
 
 # Default primary key field type
